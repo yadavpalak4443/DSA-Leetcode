6708ffe78aec895ec3b494e06ba7fd294778b5c8
Palak yadav <yadavpalak4443@gmail.com>
2026-05-16 15:42:26 +0530

    Added Two Sum solution

---
+++ Arrays/README.md
@@ -0,0 +1,10 @@
+# Two Sum - LeetCode
+
+## Approach
+Used HashMap for O(n) solution.
+
+## Time Complexity
+O(n)
+
+## Space Complexity
+O(n)
---
+++ Arrays/two_sum.py
@@ -0,0 +1,9 @@
+class solution:
+    def two_sum(self,nums,target):
+        hash_map={}
+        for i in range(len(nums)):
+            complement=target-nums[i]
+            if complement in hash_map:
+                return hash_map[complement],i
+        
+            hash_map[nums[i]]=i
